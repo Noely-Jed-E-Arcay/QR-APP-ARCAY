f786d64ea2bd78d872847295f16f2b99f112b145
Noely-Jed-E-Arcay <drixgael@gmail.com>
2026-08-12 14:41:56 +0800

    module 1-3 commit

---
+++ constants/colors.ts
@@ -0,0 +1,11 @@
+export const COLORS = {
+  primary: '#a09041',
+  background: '#35383b',
+  card: '#FFFFFF',
+  textPrimary: '#cecece',
+  textSecondary: '#c9a55e',
+  textOnPrimary: '#533711',
+  surface: '#5b6063',
+  border: '#E0E8F0',
+  shadow: '#0D47A1',
+} as const;
